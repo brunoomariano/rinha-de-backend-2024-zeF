f601f6fb7afa913d840c0a5d91f01e4af1fe5cb3
ze-fernando <jfenginner935@gmail.com>
2024-02-21 22:46:09 -0300

    feat: create db model

---
+++ db_models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import (
+    Column, Integer, String,
+    BigInteger, CHAR, ForeignKey,
+    Text, TIMESTAMP, func
+    )
+from db_config import Base
+
+
+class Cliente(Base):
+    __tablename__ = 'clientes'
+
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    limite = Column(BigInteger)
+    saldo = Column(BigInteger)
+
+
+class Transacao(Base):
+    __tablename__ = 'transacoes'
+
+    id = Column(Integer, primary_key=True)
+    valor = Column(BigInteger)
+    tipo = Column(CHAR(1))
+    descricao = Column(Text)
+    data_criacao = Column(TIMESTAMP, default=func.now())
+
+    client_id = Column(Integer, ForeignKey('clientes.id'))
