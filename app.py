de3109ba2bdf3a668d7e240a73ddaf73282709e3
ze-fernando <jfenginner935@gmail.com>
2024-02-21 22:46:41 -0300

    feat: insert, get extract

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI, HTTPException
-from list import clientes
+from fastapi import FastAPI, HTTPException, Depends
 from typing import Dict, Union
+from sqlalchemy.orm import Session
+from db_models import Cliente, Transacao
+from db_config import get_session
+from dto import ExtractDTO
+from sqlalchemy.exc import NoResultFound
 
 
 app = FastAPI()
@@ -8,36 +12,19 @@ app = FastAPI()
 
 @app.post('/clientes/{id}/transacoes', status_code=200)
 def transaction(id: int, req: Dict[str, Union[int, str]]):
-    
-    client = next((c for c in clientes if c['id'] == id), None)
-    
-    if client:
-        if(req['tipo'] == 'c'):
-            client['saldo'] -= req['valor']
-            return {
-                'limite': client['limite'],
-                'saldo': client['saldo']
-            }
-            
-        elif(req['tipo'] == 'd'):
-            if(req['valor'] <= client['limite']):
-                client['saldo'] -= req['valor']
-                return {
-                    'limite': client['limite'],
-                    'saldo': client['saldo']
-                }
-            
-        raise HTTPException(status_code=422, detail='incosistent value')
-           
+    pass
 
 
-    raise HTTPException(status_code=404, detail='client not found')
+@app.get(
+    '/clientes/{id_cliente}/extrato',
+    status_code=200,
+    response_model=ExtractDTO)
+def extrato(id_cliente: int, db: Session = Depends(get_session)):
+    try:
+        client = db.query(Cliente).filter_by(id=id_cliente).one()
+        transactions = db.query(Transacao)\
+            .filter_by(client_id=id_cliente).all()
 
-
-@app.get('/clientes/{id}/extrato')
-def extrato():
-    
-    
-    return {'msg': 'this is my first test'}
-
-
+        return {"saldo": client, "ultimas_transacoes": transactions}
+    except NoResultFound:
+        raise HTTPException(status_code=404, detail="client not found")
